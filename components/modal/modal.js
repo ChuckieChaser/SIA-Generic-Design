8ee3a16e16fb6b4a363f637d66c424922036365f
Chuckie <chuckie12602@gmail.com>
2025-12-01 15:01:38 +0800

    Generic Design v0.1

---
+++ components/modal/modal.js
@@ -0,0 +1,11 @@
+const showModal = ({ icon, heading }) => {};
+
+// *** HOW TO CALL *** //
+const successButton = $('#successButton');
+successButton.on('click', () => alert('wait lng po ginagawa pa'));
+
+const warningButton = $('#warningButton');
+warningButton.on('click', () => alert('wait lng po ginagawa pa'));
+
+const errorButton = $('#errorButton');
+errorButton.on('click', () => alert('wait lng po ginagawa pa'));
